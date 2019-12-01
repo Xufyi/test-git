bd804ec6d141766cc4905fe955f6fb02046633c6
许枫 <441784941@qq.com>
2019-12-01 11:03:03 +0800

    123

--- config/dev.env.js
+++ config/dev.env.js
@@ -7,7 +7,7 @@ module.exports = merge(prodEnv, {
   // API_HOST:'"http://192.168.100.200:88"',
   // API_HOST:'"http://192.168.100.133:8088"',//陈家
 	API_HOST:'"http://192.168.100.188:8088"',//刘虹弟
-	//API_HOST:'"http://192.168.100.172:8082"',//慧平哥
+	API_HOST:'"http://192.168.100.172:8082"',//慧平哥
   // API_HOST:'"http://192.168.100.95:8082"'WX_HOST
   // WX_HOST:'"http://192.168.100.200:8088/iqis-ws"'
 })
