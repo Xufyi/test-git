da7652470e04fed9bd570ab36f1209c0e81480c0
许枫 <441784941@qq.com>
2019-12-01 11:01:45 +0800

    edit de.js

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require('./prod.env')
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  API_HOST:'"http://192.168.100.200:88"',
+  // API_HOST:'"http://192.168.100.200:88"',
   // API_HOST:'"http://192.168.100.133:8088"',//陈家
-	// API_HOST:'"http://192.168.100.188:8088"',//刘虹弟
+	API_HOST:'"http://192.168.100.188:8088"',//刘虹弟
 	//API_HOST:'"http://192.168.100.172:8082"',//慧平哥
   // API_HOST:'"http://192.168.100.95:8082"'WX_HOST
   // WX_HOST:'"http://192.168.100.200:8088/iqis-ws"'
